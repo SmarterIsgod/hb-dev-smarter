909f98208f0aafb75e414c6ff6e4cbd1001e564e
Smarter.AdamR@gmail.com <Smarter.AdamR@gmail.com@d94c7872-a198-aad4-c608-da408a62a03c>
2012-03-10 02:45:38 +0000

    Reworking of AIMLBuddy

--- AIMLBuddy/AIMLBuddy.cs
+++ AIMLBuddy/AIMLBuddy.cs
@@ -8,63 +8,67 @@ using Styx.WoWInternals;
 
 namespace AIMLBuddy
 {
-    public class Response
+    public class AIMLBuddy : HBPlugin
     {
-        public readonly string Message;
-        public readonly string User;
-
-        public Response(string userName, string message)
-        {
-            User = userName;
-            Message = message;
-        }
-    }
-
-    public class AimlBuddy : HBPlugin
-    {
+        private bool _firstPulse = true;
         private Bot _aimlBot;
+        private List<User> _userList;
+        private List<Response> _responseList;
+        private TimeSpan _waitTime = new TimeSpan(0,0,0,3);
+
         private DateTime _lastRecieved;
         private DateTime _lastSent;
-        private List<Response> _responseList;
-        private List<User> _userList;
-        private TimeSpan _waitTime = new TimeSpan(0, 0, 0, 3);
 
-        public override string Name
+        public override void Pulse()
         {
-            get { return "AIMLBuddy"; }
+            if (_firstPulse)
+            {
+                WoWChat.Whisper += WoWChatWhisper;
+                _aimlBot = new Bot();
+                _aimlBot.loadSettings();
+                _aimlBot.loadAIMLFromFiles();
+                _userList = new List<User>();
+                _responseList = new List<Response>();
+                _lastRecieved = DateTime.Now;
+                _lastSent = DateTime.Now;
+                
+                _firstPulse = false;
+            }
+
+            if (StyxWoW.Me == null || !StyxWoW.Me.IsAlive || !StyxWoW.IsInGame || !StyxWoW.IsInWorld) return;
+
+            if (_responseList.Count <= 0) return;
+            
+            _waitTime = new TimeSpan(0, 0, 0, (int)Math.Round((_responseList.First().Message.Length * 0.22)));
+
+            if (DateTime.Now.Subtract(_lastSent) >= _waitTime && DateTime.Now.Subtract(_lastRecieved) >= _waitTime)
+            {
+                Send(_responseList.First());
+            }
         }
 
-        public override string Author
-        {
-            get { return "Smarter"; }
-        }
 
-        public override Version Version
+        private void Send(Response response)
         {
-            get { return new Version(0, 0, 2, 0); }
-        }
-
-
-        public override void Initialize()
-        {
-            WoWChat.Whisper += WoWChatWhisper;
-            _aimlBot = new Bot();
-            _aimlBot.loadSettings();
-            _aimlBot.loadAIMLFromFiles();
-            _userList = new List<User>();
-            _responseList = new List<Response>();
+            Lua.DoString(String.Format("SendChatMessage(\"{0}\", \"WHISPER\", nil, \"{1}\");", response.Message,
+                                           response.User));
+            _lastSent = DateTime.Now;
+            _responseList.Remove(response);
         }
 
         private void WoWChatWhisper(WoWChat.ChatWhisperEventArgs e)
         {
-            User user = _userList.Find(o => o.UserID == e.Author);
+            var user = _userList.Find(o => o.UserID == e.Author);
+
             if (user == null)
             {
                 user = new User(e.Author, _aimlBot);
                 _userList.Add(user);
             }
 
-            Result result = _aimlBot.Chat(new Request(e.Message, user, _aimlBot));
+            _aimlBot.GlobalSettings.updateSetting("location", StyxWoW.Me.ZoneText); //Update Zone Info BEFORE creating Response
+
+            var result = new Result(user, _aimlBot, new Request(e.Message, user, _aimlBot));
 
             var response = new Response(user.UserID, result.Output);
 
@@ -76,28 +80,20 @@ namespace AIMLBuddy
             _lastRecieved = DateTime.Now;
         }
 
-        private void TrySend(Response response)
+        public override string Name { get { return "AIMLBuddy"; } }
+        public override string Author { get { return "Smarter"; } }
+        public override Version Version { get { return new Version(0,3,0,0); }}
+    }
+
+    public class Response
+    {
+        public string User { get; private set; }
+        public string Message { get; private set; }
+
+        public Response(string user, string message)
         {
-            if (DateTime.Now.Subtract(_lastSent) >= _waitTime && DateTime.Now.Subtract(_lastRecieved) >= _waitTime)
-            {
-                Lua.DoString(String.Format("SendChatMessage(\"{0}\", \"WHISPER\", nil, \"{1}\");", response.Message,
-                                           response.User));
-                _lastSent = DateTime.Now;
-                _responseList.Remove(response);
-            }
-        }
-
-        public override void Pulse()
-        {
-            if (StyxWoW.Me == null || !StyxWoW.Me.IsAlive || !StyxWoW.IsInGame || !StyxWoW.IsInWorld) return;
-
-
-            if (_responseList.Count > 0)
-            {
-                _waitTime = new TimeSpan(0, 0, 0, (int) Math.Round((_responseList[0].Message.Length*0.22)));
-                TrySend(_responseList.First());
-                _aimlBot.GlobalSettings.updateSetting("location", StyxWoW.Me.ZoneText);
-            }
+            User = user;
+            Message = message;
         }
     }
 }
